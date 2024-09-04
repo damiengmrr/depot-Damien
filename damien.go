f520732c84c26cb66381ff08e7aef9b2a1b511f1
Damien Gamarra <damiengmrr@MacBook-Pro-de-Damien.local>
2024-09-04 16:09:43 +0200

    5e commit

--- damien.go
+++ damien.go
@@ -7,38 +7,42 @@ func zero() int {
 }
 
 func plus_one(n int) int {
-    return n + 1
+	return n + 1
 }
 
 func identity(n int) int {
 	return n
 }
 
-func max(a,b int) int {
-	if a>b {
+func max(a, b int) int {
+	if a > b {
 		return a
 	} else {
 		return b
 	}
 }
 
+func difference(a, b int) int {
+	if a > b {
 
+		return a - b
+	} else {
 
-func difference(a,b int) int {
-if a > b {
-	 
-	 return a-b 
-} else {
-	  
-	  return b-a 
- }
+		return b - a
+	}
+}
+
+func moyenne(x, y, z, t int) int {
+
+	return (x + y + z + t) / 4
 }
 
 func main() {
 	fmt.Println(zero())
-//	fmt.Println(max3(2,3,4))
-	fmt.Println(max(2,3))
-	fmt.Println(difference(1,2))
+	//	fmt.Println(max3(2,3,4))
+	fmt.Println(max(2, 3))
+	fmt.Println(difference(1, 2))
 	fmt.Println(plus_one(3))
 	fmt.Println(identity(3))
+	fmt.Println(moyenne(2, 2, 4, 4))
 }
