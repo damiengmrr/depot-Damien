d7a3485b691f3dc6ec256d32f7614c52efc15c9a
Damien Gamarra <damiengmrr@MacBook-Pro-de-Damien.local>
2024-09-04 15:19:21 +0200

    second commit

--- damien.go
+++ damien.go
@@ -7,7 +7,30 @@ func zero() int {
 }
 
 
-func main() {
-	fmt.Println(zero())
+func max(a,b int) int {
+	if a>b {
+		return a
+	} else {
+		return b
+	}
 }
 
+func max3(a int,b int,c int) int {
+	if a>b {
+		max_ab = a // donc a>b
+	} else {
+		max_ab = b // donc b>a
+	}
+	if max_ab > c {
+		return max_ab // car "A" ou "B" est > a "C"
+	} else {
+		return c // c est > a "A" et "B"
+	}
+
+}
+
+func main() {
+	fmt.Println(zero())
+	fmt.Println(max3(2,3,4))
+	fmt.Println(max(2,3))
+}
