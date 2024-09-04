d42cb92a52047b92d5ba61b920f9eb11129c80db
damiengmrr <damien.gamarra@ynov.com>
2024-09-04 21:33:28 +0200

    6e commit

--- damien.go
+++ damien.go
@@ -37,6 +37,14 @@ func moyenne(x, y, z, t int) int {
 	return (x + y + z + t) / 4
 }
 
+func invert(a int) int {
+	return -a
+}
+
+func sum(a, b int) int {
+	return a + b
+}
+
 func main() {
 	fmt.Println(zero())
 	//	fmt.Println(max3(2,3,4))
@@ -45,4 +53,7 @@ func main() {
 	fmt.Println(plus_one(3))
 	fmt.Println(identity(3))
 	fmt.Println(moyenne(2, 2, 4, 4))
+	fmt.Println(invert(5))
+	fmt.Println(sum(4, 8))
+
 }
